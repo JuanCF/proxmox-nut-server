a2e5c37076bd0c3832fe4d109bb14467042aff91
Juan Carlos Flores Martinez <jcflores_martineznajam@hotmail.com>
2026-07-04 01:14:26 -0600

    Bump v1.2.0 (#51)
    * chore: bump version to v1.2.0
    
    * chore: bump NUTWATCH_REF to v1.2.0

--- src/frontend/src/constants/index.ts
+++ src/frontend/src/constants/index.ts
@@ -105,7 +105,7 @@ export const CONFIG_FILENAMES = ['ups.conf', 'upsd.conf', 'upsmon.conf', 'upsd.u
 
 export const READONLY_CONFIG = 'upsd.users';
 
-export const APP_VERSION = 'v1.1.2';
+export const APP_VERSION = 'v1.2.0';
 
 export const MAX_LOG_LINES = 1000;
 
